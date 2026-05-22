cab5cd3390939f9524c322a0c8587c4a54ebbb5b
dany98it <56975152+dany98it@users.noreply.github.com>
2026-05-22 02:18:51 +0200

    feat: add ValueObject abstraction

---
+++ src/main/java/it/dany98/ddd_template/common/domain/ValueObject.java
@@ -0,0 +1,9 @@
+package it.dany98.ddd_template.common.domain;
+
+import it.dany98.ddd_template.common.AssertionConcern;
+
+public class ValueObject extends AssertionConcern {
+    public ValueObject() {
+        super();
+    }
+}
