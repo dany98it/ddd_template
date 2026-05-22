2781d50496201d0dc962d6ff74cf62a7b1b81247
dany98it <56975152+dany98it@users.noreply.github.com>
2026-05-22 02:09:32 +0200

    Initial commit

---
+++ src/main/java/it/dany98/ddd_template/DddTemplateApplication.java
@@ -0,0 +1,13 @@
+package it.dany98.ddd_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/dany98/ddd_template/DddTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package it.dany98.ddd_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
